8cfe6bbe5e86f89679716df8a0900198818993b9
OgabekYuldoshev <yuldashoff1@gmail.com>
2022-05-23 18:06:18 +0500

    fix

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import useList from "../modules/news/useList";
 import { ClipLoader } from "react-spinners";
 import { config } from "../../config";
+import moment from "moment";
 
 const pageVariants = {
   initial: {
@@ -30,7 +31,7 @@ const Projects = () => {
       transition={{ duration: 0.5 }}
       variants={pageVariants}
     >
-      <h1 className="text-2xl underline">News</h1>
+      <h1 className="text-2xl underline mb-8">News</h1>
 
       {isLoading && (
         <div className="my-10 flex justify-center items-center">
@@ -39,77 +40,39 @@ const Projects = () => {
       )}
 
       <div className="flex flex-col gap-5">
-        {data?.map((item, index) =>
-          !(index & 1) ? (
-            <div
-              key={index}
-              className="flex md:flex-row flex-col  gap-5 bg-white rounded-lg p-8 hover:shadow-lg hover:-translate-y-2 duration-300"
-            >
-              <div className="flex md:justify-start justify-center">
-                <div>
-                  <div className="avatar">
-                    <div className="w-32 rounded-xl">
-                      <img src={`${config.app.baseUrl}/${item.image}`} />
-                    </div>
-                  </div>
-                </div>
-              </div>
+        {data?.map((item, index) => (
+          <div
+            key={index}
+            className="flex md:flex-row flex-col  gap-5 bg-white rounded-lg p-8 hover:shadow-lg hover:-translate-y-2 duration-300"
+          >
+            <div className="flex md:justify-start justify-center">
               <div>
-                <div className="flex md:justify-start justify-center">
-                  <a
-                    href={item?.link}
-                    target="_blank"
-                    className="font-bold text-xl cursor-pointer duration border-b border-b-transparent hover:border-b-black ease-in w-fit"
-                  >
-                    {item?.title}
-                  </a>
-                </div>
-                <p className="text-gray-800  text-center md:text-left">
-                  {item?.text}
-                </p>
-              </div>
-            </div>
-          ) : (
-            <div
-              key={index}
-              className="flex md:flex-row flex-col hover:shadow-lg justify-end gap-5 bg-white rounded-lg p-8 transform hover:-translate-y-2 duration-300"
-            >
-              <div className="md:hidden flex md:justify-start justify-center">
-                <div>
-                  <div className="avatar">
-                    <div className="w-32 rounded-xl">
-                      <img src={`${config.app.baseUrl}/${item.image}`} />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div className="flex md:justify-end justify-center">
-                  <a
-                    href={item?.link}
-                    className="font-bold text-right cursor-pointer text-xl duration border-b border-b-transparent hover:border-b-black ease-in w-fit"
-                  >
-                    {item?.title}
-                  </a>
-                </div>
-
-                <p className="text-gray-800 md:text-right text-center">
-                  {item?.text}
-                </p>
-                <span>{moment(item?.created_at).format("LLLL")}</span>
-              </div>
-              <div className="md:flex hidden">
-                <div>
-                  <div className="avatar">
-                    <div className="w-32 rounded-xl">
-                      <img src={`${config.app.baseUrl}/${item.image}`} />
-                    </div>
+                <div className="avatar">
+                  <div className="w-[300px] rounded-xl">
+                    <img src={`${config.app.baseUrl}/${item.image}`} />
                   </div>
                 </div>
               </div>
             </div>
-          )
-        )}
+            <div>
+              <div className="flex md:flex-row flex-col md:justify-between justify-center mb-5">
+                <a
+                  href={item?.link}
+                  target="_blank"
+                  className="font-bold text-xl cursor-pointer duration border-b border-b-transparent hover:border-b-black ease-in w-fit"
+                >
+                  {item?.title}
+                </a>
+                <span className="mt-2">
+                  {moment(item?.created_at).format("LLLL")}
+                </span>
+              </div>
+              <p className="text-gray-800  text-center md:text-left">
+                {item?.text}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </motion.div>
   );
