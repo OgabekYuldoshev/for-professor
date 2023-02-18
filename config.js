c2e5746a2e8cbb94f12065734b84a605cc7c2f68
Nozy12 <79836552+Nozy12@users.noreply.github.com>
2023-02-18 14:10:11 +0500

    change3

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
     app: {
         name: 'Ogabek Yuldoshev',
-        baseUrl: "http://admin.khojamatov.uz/public"
+        baseUrl: "http://admin.khojamatov.uz"
     },
 }
